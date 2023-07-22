90c7c02edca3172b7e57fb18342974a3f885dca9
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-22 14:12:14 +0000

    copied program

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
+// Function to find the minimum value of an array
+
 #include <stdio.h>
 
+// Declare functions
+int minimum (int[]);
+
 int main(void) {
-  printf("Hello World\n");
+
+  int scores[10], i, minScore;
+  int minimum (int values[10]);
+
+  // Get input scores
+  printf ("Enter 10 scores\n");
+  for (int i = 0; i <10; i++) {
+    scanf ("%d", &scores[i]);
+  }
+
+  // Calculate and print minimum score
+  minScore = minimum (scores);
+  printf ("\nMinimum score is %d\n", minScore);
+  
   return 0;
+}
+
+// Define functions
+int minimum (int values[10]) {
+  int minValue, i;
+
+  minValue = values[0];
+
+  for (i = 1; i < 10; i++) {
+    if (values[i] < minValue) {
+      minValue = values [i];
+    }
+  }
+  return minValue;
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Programming in C
+## Chapter 8
+### Question 1: Part 9
+
+This problem is from chapter 8 of the textbook[^1]. It is to copy program 8.9[^2] from the textbook. This program finds the minimum value of an array.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 162 - Question 1 Part 9
+[^2]: Programming in C (Third edition) By Stephen G. Kochan - Page 138 - Program 8.9
